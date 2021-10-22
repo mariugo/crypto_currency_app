c1b1d26c0d9dd0a19842d197f7ad63364c92c7b9
Mariugo Megiani <mariugomegiani@gmail.com>
2021-10-22 18:09:31 -0300

    Coin Repository implementation

---
+++ app/src/main/java/com/mariugo/cryptocurrency/domain/repository/CoinRepository.kt
@@ -0,0 +1,11 @@
+package com.mariugo.cryptocurrency.domain.repository
+
+import com.mariugo.cryptocurrency.data.remote.dto.CoinDetailDto
+import com.mariugo.cryptocurrency.data.remote.dto.CoinDto
+
+interface CoinRepository {
+
+    suspend fun getCoins() : List<CoinDto>
+
+    suspend fun getCoinById() : CoinDetailDto
+}
